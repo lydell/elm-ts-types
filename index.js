3f81706cf7e3ea4146a12ef73822fb35a1409451
Simon Lydell <simon.lydell@gmail.com>
2026-06-19 23:37:08 +0200

    Code cleanups

--- index.js
+++ index.js
@@ -95,24 +95,22 @@ var console = {
 `.trim();
 
 const injection = `
-var proxy = new Proxy({}, {
-    get: function () {
+const proxy = new Proxy({}, {
+    get() {
         return proxy;
     }
 });
 
-function _Platform_initialize(flagDecoder, args, init, update, subscriptions, stepperBuilder)
-{
-	var ports = _Platform_setupEffects({}, function () {});
+function _Platform_initialize(flagDecoder, args, init, update, subscriptions, stepperBuilder) {
+    const ports = _Platform_setupEffects({}, () => {});
     return {
         flagDecoder: flagDecoder.$ === 0 ? null : decoderToString(flagDecoder),
-        ports: ports || null,
-        usesNodeOption: stepperBuilder.toString().indexOf("'node'") !== -1,
+        ports: ports ?? null,
+        usesNodeOption: stepperBuilder.toString().includes("'node'"),
     };
 }
 
-var _VirtualDom_init = F4(function(virtualNode, flagDecoder, debugMetadata, args)
-{
+var _VirtualDom_init = F4(function(virtualNode, flagDecoder, debugMetadata, args) {
     return {
         flagDecoder: null,
         ports: null,
@@ -121,68 +119,52 @@ var _VirtualDom_init = F4(function(virtualNode, flagDecoder, debugMetadata, args
 });
 
 function _Platform_setupIncomingPort(name) {
-	return {
+    return {
         type: "incoming",
         value: decoderToString(_Platform_effectManagers[name].u())
     };
 }
 
 function _Platform_setupOutgoingPort(name) {
-	return {
+    return {
         type: "outgoing",
         value: _Json_unwrap(_Platform_effectManagers[name].u(proxy))
     };
 }
 
-var $elm$core$Basics$identity = function () { return _Json_wrap("unknown"); };
-
+var $elm$core$Basics$identity = () => _Json_wrap("unknown");
 var $elm$json$Json$Encode$null = _Json_wrap("null");
+var $elm$json$Json$Encode$bool = () => _Json_wrap("boolean");
+var $elm$json$Json$Encode$int = () => _Json_wrap("number");
+var $elm$json$Json$Encode$float = () => _Json_wrap("float");
+var $elm$json$Json$Encode$string = () => _Json_wrap("string");
 
-var $elm$json$Json$Encode$bool = function () {
-    return _Json_wrap("boolean");
-};
-
-var $elm$json$Json$Encode$int = function () {
-    return _Json_wrap("number");
-};
-
-var $elm$json$Json$Encode$float = function () {
-    return _Json_wrap("float");
-};
-
-var $elm$json$Json$Encode$string = function () {
-    return _Json_wrap("string");
-};
-
-var $elm$json$Json$Encode$list = F2(
-	function (func, entries) {
-        if (func === $elm$core$Basics$identity) {
-            var output = "[";
-            for (; entries.b; entries = entries.b) // WHILE_CONS
-			{
-				output += _Json_unwrap(entries.a) + ", ";
-			}
-            output += "]";
-			return _Json_wrap(output);
-        } else {
-            return _Json_wrap("Array<" + _Json_unwrap(func(proxy)) + ">");
+var $elm$json$Json$Encode$list = F2(function (func, entries) {
+    if (func === $elm$core$Basics$identity) {
+        let output = "[";
+        for (; entries.b; entries = entries.b) // WHILE_CONS
+        {
+            output += _Json_unwrap(entries.a) + ", ";
         }
-	});
-
-var $elm$json$Json$Encode$array = F2(
-	function (func, entries) {
+        output += "]";
+        return _Json_wrap(output);
+    } else {
         return _Json_wrap("Array<" + _Json_unwrap(func(proxy)) + ">");
-	});
+    }
+});
+
+var $elm$json$Json$Encode$array = F2(function (func, entries) {
+    return _Json_wrap("Array<" + _Json_unwrap(func(proxy)) + ">");
+});
 
 var $elm$json$Json$Encode$object = function (pairs) {
-	var object = A3(
+    var object = A3(
         $elm$core$List$foldl,
-        F2(
-            function (_v0, obj) {
-                var k = _v0.a;
-                var v = _v0.b;
-                return A3(_Json_addField, k, v, obj);
-            }),
+        F2(function (_v0, obj) {
+            var k = _v0.a;
+            var v = _v0.b;
+            return A3(_Json_addField, k, v, obj);
+        }),
         _Json_emptyObject(_Utils_Tuple0),
         pairs
     );
@@ -194,14 +176,12 @@ var $elm$json$Json$Encode$object = function (pairs) {
     return _Json_wrap(output);
 };
 
-var $elm$core$Maybe$destruct = F3(
-	function (_default, func, maybe) {
-        return _Json_wrap(_Json_unwrap(func(proxy)) + " | " + _Json_unwrap(_default));
-	});
+var $elm$core$Maybe$destruct = F3(function (_default, func, maybe) {
+    return _Json_wrap(_Json_unwrap(func(proxy)) + " | " + _Json_unwrap(_default));
+});
 
-var $elm$json$Json$Decode$andThen = F2(function(callback, decoder)
-{
-    var next = callback(proxy);
+var $elm$json$Json$Decode$andThen = F2(function(callback, decoder) {
+    const next = callback(proxy);
     if (next.$ === 0) {
         return decoder;
     }
@@ -218,7 +198,6 @@ var $elm$json$Json$Decode$float = { $: "primitive", value: "number" };
 var $elm$json$Json$Decode$string = { $: "primitive", value: "string" };
 var $elm$json$Json$Decode$value = { $: "primitive", value: "unknown" };;
 
-
 var $elm$json$Json$Decode$list = function (decoder) {
     return { $: "list", value: decoder };
 };
@@ -227,31 +206,27 @@ var $elm$json$Json$Decode$array = function (decoder) {
     return { $: "array", value: decoder };
 };
 
-var $elm$json$Json$Decode$field = F2(function(field, decoder)
-{
+var $elm$json$Json$Decode$field = F2(function(field, decoder) {
     var object = {};
     object[field] = decoder;
     return { $: "object", value: object };
 });
 
-var $elm$json$Json$Decode$index = F2(function(index, decoder)
-{
+var $elm$json$Json$Decode$index = F2(function(index, decoder) {
     var object = {};
     object[index] = decoder;
     return { $: "tuple", value: object };
 });
 
-var $elm$json$Json$Decode$oneOf = function (decoders)
-{
-	return {
-		$: "oneOf",
-		alternatives: _List_toArray(decoders)
-	};
-}
+var $elm$json$Json$Decode$oneOf = function (decoders) {
+    return {
+        $: "oneOf",
+        alternatives: _List_toArray(decoders)
+    };
+};
 
-var $elm$json$Json$Decode$map = F2(function(f, d1)
-{
-	return d1;
+var $elm$json$Json$Decode$map = F2(function(f, d1) {
+    return d1;
 });
 
 function decoderToString(decoder) {
